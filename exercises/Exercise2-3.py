84c91dab60e232b00d02a3fe1e8f50d7b9ce00c0
Ryan Carr <rycarr@gmail.com>
2019-02-03 20:50:35 -0600

    Added Chapter 2 exercises

---
+++ exercises/Exercise2-3.py
@@ -0,0 +1,5 @@
+hours = float(input('Enter hours: '))
+rate = float(input('Enter rate: '))
+pay = hours * rate
+
+print('Pay:', pay)
---
+++ exercises/Exercise2-5.py
@@ -0,0 +1,3 @@
+celsius = float(input('Enter a temperature in Celsius: '))
+farenheit = ((9/5.0) * celsius) + 32
+print(str(celsius) + 'C is', str(farenheit) + 'F')
